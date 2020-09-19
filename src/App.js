f3e2952cd8274af9969c828512d6ae22cde3ad57
Eirik Kalvø Vattøy <eirikkalven@gmail.com>
2020-09-19 13:51:33 +0200

    Tabel & pagination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Posts } from "./components/Posts";
+import { Pagination } from "./components/Pagination";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(20);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(
+          "https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=100"
+        );
+        const data = await res.json();
+        setPosts(data.items);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // get current posts
+  const indexOflastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOflastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOflastPost);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Most popular js repositories</h1>
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
+      <Posts posts={currentPosts} loading={loading} />
     </div>
   );
 }
---
+++ src/components/Posts.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Loader } from "./Loader";
+
+import "./posts.scss";
+
+export const Posts = ({ posts, loading }) => {
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <div className="postContainer">
+      <div className="overviewRepo">
+        {/* repositories, Forked , watchers , Homepage  */}
+        <p>Repositories</p>
+        <p>Forked</p>
+        <p>Watchers</p>
+        <p>Homepage</p>
+      </div>
+      {posts.map((post) => {
+        return (
+          <div className="repositories" key={post.id}>
+            <h2>{post.name}</h2>
+            <span> {post.forks}</span>
+            <span> {post.watchers}</span>
+            <a href={post.homepage}>{post.name}</a>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
